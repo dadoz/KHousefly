70c469e276f68e439368ed113a1b47d211c58e77
davide-ennova <davide.lamanna@ennova.it>
2017-03-19 16:33:28 +0100

    major fix to start kotlink compiling app

---
+++ app/src/main/kotlin/com/kotlin/application/davidelm/khousefly/MainActvity.kt
@@ -0,0 +1,18 @@
+package com.kotlin.application.davidelm.khousefly
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initView()
+    }
+
+    private fun initView() {
+        Log.e("TAG", "hey")
+    }
+}
